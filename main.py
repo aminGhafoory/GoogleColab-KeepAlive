b2322cb14381edf031a20a67249174dee0ceb0e9
AminGhafoori <ghafooriamin3@gmail.com>
2021-09-08 20:55:54 +0430

    'daa'

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ import time
 from random_qoute import random_qoute
 
 
+
 screenWidth, screenHeight = pyautogui.size()
 #(162,518)
 print('please point the mouse on the target point and press Crtl+C ')
 
 try:
     while True:
-        currentMouseX, currentMouseY = pyautogui.position()
-        print(currentMouseX,currentMouseY,end="\r")
+        targetMouseX, targetMouseY = pyautogui.position()
+        print(targetMouseX,targetMouseY,end="\r")
         time.sleep(0.1)
 except KeyboardInterrupt:
-    print(f'target Point is {currentMouseX},{currentMouseY}')
+    print(f'target Point is {targetMouseX},{targetMouseY}')
 
-
+#set pause
+pyautogui.PAUSE = 2.5
 
 #move mouse to target position and send random text
+pyautogui.moveTo(targetMouseX, targetMouseY, duration=1)
 
+#get a random qoute in list format
 random_qoute_list = random_qoute().split(' ')
+
 
 
 
--- random_qoute.py
+++ random_qoute.py
@@ -1,10 +1,13 @@
 import requests
 import json
+import random
+
+random_num = random.randint(1,30)
 
 def random_qoute()-> str:
     '''gets a random qoute'''
     res = requests.get('https://type.fit/api/quotes')
     res = json.loads(res.text)
-    print(res[0]['text']) 
+    return res[random_num]['text'] 
 
-random_qoute()
+
