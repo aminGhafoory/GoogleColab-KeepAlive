0b7e47cf73a1cfcd08b8ba97bf2f2d598c9b4852
AminGhafoori <ghafooriamin3@gmail.com>
2021-09-08 20:40:29 +0430

    'mouse_position_finder'

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pyautogui
+import time
+
+screenWidth, screenHeight = pyautogui.size()
+#(162,518)
+print('please point the mouse on the target point and press Crtl+C ')
+
+try:
+    while True:
+        currentMouseX, currentMouseY = pyautogui.position()
+        print(currentMouseX,currentMouseY,end="\r")
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print(f'target Point is{currentMouseX},{currentMouseY}')
+
+
+
