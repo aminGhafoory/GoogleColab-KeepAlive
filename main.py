0d58275919578f9eded8eef2c9027a3fdc430308
AminGhafoury <ghafooriamin3@gmail.com>
2021-10-18 13:33:42 +0330

    added local db

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import pyautogui
 import time
-from random_qoute import random_qoute
+from random_qoute import write_in_txt
 import random
 
 
 screenWidth, screenHeight = pyautogui.size()
 
-print('please point the mouse on the target point and press Crtl+C ')
+print("please point the mouse on the target point and press Crtl+C ")
 
 try:
     while True:
         targetMouseX, targetMouseY = pyautogui.position()
-        print(targetMouseX,targetMouseY,end="\r")
+        print(targetMouseX, targetMouseY, end="\r")
         time.sleep(0.1)
 except KeyboardInterrupt:
-    print(f'target Point is {targetMouseX},{targetMouseY}')
+    print(f"target Point is {targetMouseX},{targetMouseY}")
 
 
-#move mouse to target position and click
+# move mouse to target position and click
 pyautogui.moveTo(targetMouseX, targetMouseY, duration=1)
 
 pyautogui.click()
 
+write_in_txt("qoutes")
+qoutes_set = {x.rstrip() for x in open("qoutes.txt")}
+len_qoutes_set = len(qoutes_set)
+
 while True:
-    #get a random qoute in list format
-    random_qoute_list = random_qoute().split(' ')
-    random_interval = random.random()
+    # get a random qoute in list format
+    random_qoute = list(qoutes_set)[random.randint(0, len_qoutes_set)]
+    random_qoute_list = random_qoute.split(" ")
+
     for word in random_qoute_list:
-        pyautogui.write(f'{word} ', interval=random_interval/4.)
-    pyautogui.hotkey('ctrl', 'a', 'backspace')
-    
-        
-
-
-
+        random_interval = random.random()
+        pyautogui.write(f"{word} ", interval=random_interval / 3.0)
+    pyautogui.hotkey("ctrl", "a", "backspace")
--- random_qoute.py
+++ random_qoute.py
@@ -1,14 +1,17 @@
 import requests
 import json
-import random
 
 
 
-def random_qoute()-> str:
-    '''gets a random qoute'''
-    random_num = random.randint(0,30)
-    res = requests.get('https://type.fit/api/quotes')
+def random_qoute() -> str:
+    """gets a random qoute"""
+    res = requests.get("https://type.fit/api/quotes")
     res = json.loads(res.text)
-    return res[random_num]['text'] 
+    return res
 
 
+def write_in_txt(output_filename: str = "qoutes") -> None:
+    res = random_qoute()
+    with open(f"{output_filename}.txt", "a") as f:
+        for index, q in enumerate(res):
+            f.write(f'{res[index]["text"]}\n')
